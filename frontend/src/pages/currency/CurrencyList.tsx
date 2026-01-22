fe4f0efbb2f9e65cac2d5648e4bef61888aed977
webdeveloper@wsl <webdeveloper@wsl>
2026-01-22 16:58:11 +0500

    added new filters

--- frontend/src/pages/currency/CurrencyList.tsx
+++ frontend/src/pages/currency/CurrencyList.tsx
@@ -1,19 +1,12 @@
-import { useCurrencyStore } from '@/entities/currency/lib/useCurrencyStore'
+import { useCurrencyList } from '@/entities/currency/lib/useCurrencyList'
 import { TCurrency } from '@/entities/currency/types/currency.types'
 import MyTable from '@/shared/components/ui/MyTable/MyTable'
 
 const currencyHeaders = [<>Name</>, <>Code</>, <>Symbol</>]
 
 const CurrencyList = () => {
-    const { currencies, loadCurrencies } = useCurrencyStore()
-    if (!currencies || currencies.length < 1) {
-        return (
-            <div className="flex gap-x-2">
-                <p>Нет доступных валют.</p>
-                <button onClick={loadCurrencies}>Обновить</button>
-            </div>
-        )
-    }
+    const { data: currencies = [] } = useCurrencyList()
+
     const renderCurrenciesRow = (currency: TCurrency) => [
         <>{currency.name}</>,
         <>{currency.code}</>,
